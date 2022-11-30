121fafe3231769f87899c102e61779c28612e41f
Alexandre Caramel <alexandrecaramel@MacBook-Pro-de-Alexandre.local>
2022-11-30 16:16:55 +0100

    add list users

--- src/views/map/Map.js
+++ src/views/map/Map.js
@@ -4,22 +4,26 @@ import ListRestaurant from './partials/ListRestaurant';
 import ListUser from './partials/ListUser';
 import MapLeaflet from './partials/MapLeaflet';
 import socketIO from 'socket.io-client';
+import {useParams} from "react-router-dom";
 const socket = socketIO.connect('http://localhost:4000');
 
 const Map = () =>  {
+  const {id} = useParams()
+  const [room, setRoom] = useState([])
   const [dataRoom, setDataRoom] = useState([])
   useEffect(() => {
     socket.on('dataRoomResponse', (data) => setDataRoom(data))
+    setRoom(dataRoom.filter(room => room.idRoom === parseInt(id)))
   }, [socket, dataRoom])
 
   return (
     <div className={'section-map-container'} >
       <h4>Rendez-vous à 13h : tu dois partir à 8h</h4>
       <ListRestaurant />
-      <MapLeaflet dataRoom={dataRoom}/>
+      <MapLeaflet room={room}/>
       <div className='container-right'>
         <button className='button-leave'>QUITTER</button>
-        <ListUser />
+        <ListUser room={room} />
         <Chatbox />
       </div>
     </div>
--- src/views/map/partials/ListUser.js
+++ src/views/map/partials/ListUser.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
-export const ListUser = () =>  (
+export const ListUser = (room) => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    if(room.room.length > 0) {
+      setUsers(room.room[0]["users"])
+    }
+
+  }, [room.room])
+
+  return(
     <div className={"list-user"}>
-        <h3>Liste des utilisateurs</h3>
-        <ul>
-            <li>Karen</li>
-            <li>Alexandre</li>
-            <li>Benjamin</li>
-            <li>Nathan</li>
-            <li>Louis-Axel</li>
-        </ul>
+      <h3>Liste des utilisateurs</h3>
+      <ul>
+        {
+          users.map(user => (<li>{user.name}</li>))
+        }
+      </ul>
     </div>
-)
-
+    )
+}
 
 export default ListUser;
